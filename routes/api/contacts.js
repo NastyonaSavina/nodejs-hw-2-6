3f5a089dcb315eb31a5d837a18dba0e214d3f3b8
NastyonaSavina <nastyona.savina@gmail.com>
2023-03-19 21:47:27 +0200

    base

---
+++ routes/api/contacts.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const {
+  getContacts,
+  getContactDataById,
+  addNewContact,
+  deleteContactById,
+  updateContactById
+} = require('../../controllers/contactsController');
+
+const { asyncWrapper } = require('../../helpers/apiHelpers');
+
+const {addContactValidation } = require('../../middlewares/validationMiddleware');
+
+const router = express.Router();
+
+router.get('/', asyncWrapper(getContacts));
+
+router.get('/:contactId', asyncWrapper(getContactDataById));
+
+router.post('/', addContactValidation, asyncWrapper(addNewContact));
+
+router.delete('/:contactId', asyncWrapper(deleteContactById));
+
+router.put('/:contactId', addContactValidation, asyncWrapper(updateContactById) )
+
+module.exports = router;
