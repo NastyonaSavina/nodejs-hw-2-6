cdd3ad21c9f80ab688f85c7ca7e784a981fe9c7f
NastyonaSavina <nastyona.savina@gmail.com>
2023-04-08 14:32:46 +0300

    addition

--- controllers/contacts/getContactsController.js
+++ controllers/contacts/getContactsController.js
@@ -4,8 +4,9 @@ const { getListContacts } = require("../../service/contacts/contactsService");
 
 const getContactsController = async (req, res, next) => {
     const { _id: owner } = req.user;
+    const { page = 1, limit = 20 } = req.query;
 
-    const contacts = await getListContacts(owner);
+    const contacts = await getListContacts(owner, page, limit);
     res.status(200).json({contacts});
 };
 
